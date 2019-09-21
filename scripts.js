aa6ecf2428b0cc44f3db15015c2c9db17b65fe0e
Robert Lee <bobbydwise1@hotmail.com>
2019-09-20 18:12:59 -0700

    Finish UI and bussiness logic.

--- scripts.js
+++ scripts.js
@@ -8,17 +8,18 @@ Given a positive integer n, return the n-th perfect number.
 For example, given 1, you should return 19. Given 2, you should return 28.
 */
 
-
+const findTen = (yourNumber) => {
+  let output = 10 - yourNumber
+  return yourNumber + output
+}
 
 $(document).ready(function() {
 
   $('#form-1').submit(function(){
     event.preventDefault()
     input1 = $('#input-1').val()
-    input1 = input1.replace(/\'/g,'"')
-    input2 = JSON.parse(input1)
     console.log(input1)
-    $('#output-1').text(findProduct(input2))
+    $('#output-1').text(findTen(input1))
   })
 
 });
